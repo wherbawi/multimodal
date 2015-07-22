52a91f55c94eee75df9f6d445c966a6668a7cd17
unknown <WHERBAW@C527L0039036894.dms.corpintra.net>
2015-07-22 11:38:39 +0200

    packages renaming. recording removal for old names

--- src/main/java/herbawi/wesam/multimodal/domain/StreetVehicleEdge.java
+++
@@ -1,9 +0,0 @@
-package herbawi.wesam.multimodal.domain;
-
-public class StreetVehicleEdge extends Edge{
-
-	public StreetVehicleEdge(int destinationId, int duration) {
-		super(destinationId, duration);
-	}
-
-}
--- src/main/java/herbawi/wesam/multimodal/domain/VehicleStreetEdge.java
+++
@@ -1,9 +0,0 @@
-package herbawi.wesam.multimodal.domain;
-
-public class VehicleStreetEdge extends Edge {
-
-	public VehicleStreetEdge(int destinationId, int duration) {
-		super(destinationId, duration);
-	}
-
-}
